0a85d0e8380a678a2832baedb746f919d7ca904a
Awanis Azizan <awanis.azizan@pslgroup.com>
2018-10-31 11:27:26 +0800

    Conditions

---
+++ shipping.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:20:04 2018
+
+@author: awanis.azizan
+"""
+
+answer = input("Would you like express shipping?")
+if answer == "yes" :    
+    print("That would add $10 to the fees")
+    print("Thank you!") 
+else :    
+    print("Thank you for your purchase!")    
