480667bc44f31cfe761ec990942f4f2649a56898
Awanis Azizan <awanis.azizan@pslgroup.com>
2018-10-31 11:40:09 +0800

    String to Number

---
+++ strToNum.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:36:32 2018
+
+@author: awanis.azizan
+"""
+
+deposit = input("How much would you like to deposit?")
+if float(deposit) > 100:
+    print("You get a free toaster!")
+
+print("Have a nice day!")    
