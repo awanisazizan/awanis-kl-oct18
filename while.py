e7067c4dd597f3c959f9ef18e31b2531ec38fcc0
Awanis Azizan <awanis.azizan@pslgroup.com>
2018-11-01 11:23:07 +0800

    while

---
+++ while.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 11:13:56 2018
+
+@author: awanis.azizan
+"""
+
+answer = 0
+while answer != 4:
+    answer = int(input("What is 2+2? "))
+    
+print("Yes! 2+2 = 4")
