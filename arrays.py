fac59c9910e00c4c7b47b45b99b1a0a9835029dc
Awanis Azizan <awanis.azizan@pslgroup.com>
2018-11-01 11:57:47 +0800

    Arrays

---
+++ arrays.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 11:34:58 2018
+
+@author: awanis.azizan
+"""
+
+guest = ["first","second","third"]
+#print('First value default:' +guest[0])
+#print(guest[-2])
+
+#guest[0] = 'Steve'
+#print('First value now:' +guest[0])
+
+#guest.append('New Guy')
+#print('The new value now:' +guest[-1])
+
+#guest.remove('second')
+#print('Second element is:' +guest[1])
+
+
+scores = ['90','91','92','93','94']
+#print(scores[-2])
+
+
+for step in range(len(guest)):
+    print(guest[step])
