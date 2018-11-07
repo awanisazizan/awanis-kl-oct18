79b58665a652fa8d6a96d7bdc66fc5a40bcb2acd
Awanis Azizan <awanis.azizan@pslgroup.com>
2018-11-07 10:33:07 +0800

    csv

---
+++ csvsample1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 10:15:59 2018
+
+@author: awanis.azizan
+"""
+
+import csv
+#
+#myCSVFile = ('python-kl-oct18/data/demo.csv','r' )
+#dataFromFile = (csv.reader, myCSVFile)
+fileName = 'GuestList.txt'
+accessMode = 'r'
+
+with open(fileName, accessMode) as myCSVFile:
+    dataFromFile = (csv.reader, myCSVFile)
+
+#print(dataFromFile)
+
+for row in dataFromFile:
+    print(row)
