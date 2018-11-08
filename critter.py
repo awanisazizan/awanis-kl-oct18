124f323e0e692727c72a067628f31c01067ea238
Awanis Azizan <awanis.azizan@pslgroup.com>
2018-11-08 11:06:30 +0800

    critter

---
+++ critter.py
@@ -0,0 +1,11 @@
+class Critter:
+    
+    #def __init__(self, hello) :
+     #   self.hello = hello
+
+    def talk(self) : 
+        print("Hi. I'm an instance of a class Critter.") 
+
+
+crit1 = Critter()
+crit1.talk()
